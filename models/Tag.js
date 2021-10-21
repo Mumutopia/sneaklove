152751c621ffed380c71877f328482572f6442b5
mumutopia <lemulot78@hotmail.com>
2021-10-21 14:58:11 +0200

    removed some s...

---
+++ models/Tag.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const tagSchema = new Schema({
+    label: String,
+});
+
+const TagModel = mongoose.model("tags", tagSchema)
+module.exports = TagModel
