0a3c3c4dd4ed9cf858ac28c3e3bdde7e3d7f0d07
Constance <constance@MacBook-Pro-de-Constance.local>
2021-10-22 11:52:46 +0200

    CRUD almost ok

--- routes/dashboard_sneaker.js
+++ routes/dashboard_sneaker.js
@@ -1,4 +1,5 @@
 const express = require("express"); // import express in this module
+const { findByIdAndUpdate } = require("../models/Sneaker");
 const SneakerModel = require("../models/Sneaker");
 const router = new express.Router(); // create an app sub-module (router)
 const protectPrivateRoute = require("./../middlewares/protectPrivateRoute")
@@ -17,6 +18,15 @@ router.get("/prod-add", protectPrivateRoute, (req, res) => {
   res.render("products_add.hbs");
 });
 
+router.post("/prod-add", protectPrivateRoute, async (req,res) => {
+  try{
+  await SneakerModel.create(req.body)
+  res.redirect("/prod-manage")
+  }catch (err) {
+    next(err)
+  }
+})
+
 router.get("/prod-manage/:id",protectPrivateRoute, async (req, res) => {
   try {
     await SneakerModel.findByIdAndDelete(req.params.id)
@@ -25,5 +35,29 @@ router.get("/prod-manage/:id",protectPrivateRoute, async (req, res) => {
     next(err);
   }  
 });
+
+
+//update
+router.get("/prod-edit/:id",protectPrivateRoute, async (req, res) => {
+  try {
+    const sneaker = await SneakerModel.findById(req.params.id);
+    res.render("product_edit.hbs", {sneaker});
+  }catch (err) {
+    next(err);
+  }  
+});
+
+router.post("/prod-edit/:id", protectPrivateRoute, async (req, res) => {
+  try {
+    await SneakerModel.findByIdAndUpdate(req.params.id, req.body)
+    res.redirect("/prod-manage")
+  }
+  catch (err) {
+  next(err);
+}
+});
+
+
+
 
 module.exports = router;
